ea7b260f6f43209a618cb315cba6dc7968885a0c
Mariam Hasanat <201011@ppu.edu.ps>
2023-12-28 20:44:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import { useState } from 'react';
+import './App.css';
+// import Form from './components/form';
+// import ToDoInput from './components/to-do';
+// import ToDoList from './components/to-do-list';
+
+function App() {
+  // const [array, setArray] = useState([])
+  return (
+    <div className="App">
+
+      {/* <Form/> */}
+
+     {/* <ToDoInput array = {array} setArray = {setArray} />
+     <ToDoList array = {array} /> */}
+    </div>
+  );
+}
+
+export default App;
