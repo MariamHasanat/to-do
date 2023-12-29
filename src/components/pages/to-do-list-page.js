8d45f112a168a34ad63aa967deed0c237a69c510
Mariam Hasanat <201011@ppu.edu.ps>
2023-12-29 15:27:39 +0200

    changes in elements type as well as add classNames for styles

--- src/components/pages/to-do-list-page.js
+++ src/components/pages/to-do-list-page.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ToDoInput from '../to-do-input'
 import ToDoElement from '../to-do-element'
-
+import './to-do-page/todo.css'
 
 const ToDoList = () => {
 
@@ -12,16 +12,26 @@ const ToDoList = () => {
   }, [array])
 
   return (
-    <div>
-      <ToDoInput array={array} setArray={setArray} />
-      {
-        array.map((value, index) => (
-          <div key={index}>
-            <ToDoElement to_do_value={value} index={index} array={array} setArray={setArray} />
+    <div className='body'>
+      <div className="container1">
+        <div className="todo-app">
+          <div className="app-title">
+            <h2>To-do app</h2>
+            <i className="fa-solid fa-book-bookmark"></i>
           </div>
-        )
-        )
-      }
+          <ToDoInput array={array} setArray={setArray} />
+          <ul id="list-container">
+            {
+              array.map((value, index) => (
+                <ul key={index}>
+                  <ToDoElement to_do_value={value} index={index} array={array} setArray={setArray} />
+                </ul>
+              )
+              )
+            }
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
